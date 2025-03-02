e1d9321d3195bde1492c20ab5d62b83599c802cd
Kamil Glazer <k.glazer.kamil@interia.pl>
2025-03-02 17:45:58 +0100

    Login and register page

--- zentro-backend/src/main/java/com/kamilglazer/Zentro/config/ApplicationConfig.java
+++ zentro-backend/src/main/java/com/kamilglazer/Zentro/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.kamilglazer.Zentro.config;
 
 import com.kamilglazer.Zentro.exception.UserNotFoundException;
 import com.kamilglazer.Zentro.repository.UserRepository;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,6 +14,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
@@ -44,6 +47,20 @@ public class ApplicationConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
+
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            public void addCorsMappings(@NonNull CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("*")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                        .allowedHeaders("*")
+//                        .exposedHeaders("Authorization")
+//                        .allowCredentials(true);
+//            }
+//        };
+//    }
 
 
 }
